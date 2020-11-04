6d77a36ec1ecaddbfa13e5a8fd025208adde2728
Alexandre Pantalena Yoshimatsu <aleyoshimatsu@gmail.com>
2020-11-03 22:27:42 -0300

    projeto inicial

---
+++ src/test/java/br/ce/wcaquino/taskbackend/controller/TaskControllerTest.java
@@ -0,0 +1,56 @@
+package br.ce.wcaquino.taskbackend.controller;
+
+import static io.restassured.RestAssured.*;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.matcher.RestAssuredMatchers.*;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.Matchers.*;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TaskControllerTest {
+
+    @BeforeClass
+    public static void setup() {
+        baseURI = "http://localhost:8081/tasks-backend";
+    }
+
+    @Test
+    public void deveRetornarTarefas() {
+        given()
+        .when()
+            .get("/todo")
+        .then()
+            .statusCode(200)
+        ;
+    }
+
+    @Test
+    public void deveAdicionarTarefaComSucesso() {
+        given()
+            .body("{\"task\": \"Teste via API\", \"dueDate\": \"2020-12-30\"}")
+            .contentType(ContentType.JSON)
+        .when()
+            .post("/todo")
+        .then()
+            .log().all()
+            .statusCode(201)
+        ;
+    }
+
+    @Test
+    public void naoDeveAdicionarTarefaInvalida() {
+        given()
+                .body("{\"task\": \"Teste via API\", \"dueDate\": \"2010-12-30\"}")
+                .contentType(ContentType.JSON)
+        .when()
+                .post("/todo")
+        .then()
+                .log().all()
+                .statusCode(400)
+                .body("message", CoreMatchers.is("Due date must not be in past"))
+        ;
+    }
+}
